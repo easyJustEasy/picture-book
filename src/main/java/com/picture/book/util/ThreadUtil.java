ca9bef54565ecfbb01dcc86a088465692300e82e
siyuan.zhu <zhusiyuanhao@163.com>
2025-02-14 16:57:43 +0800

    ii

--- src/main/java/com/picture/book/util/ThreadUtil.java
+++ src/main/java/com/picture/book/util/ThreadUtil.java
@@ -7,12 +7,12 @@ import java.util.concurrent.TimeUnit;
 public class ThreadUtil {
     //创建一个线程池
     private static final ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
-            10,
-            20,
+            50,
+            100,
             60,
             TimeUnit.SECONDS,
             new ArrayBlockingQueue<>(100),
-            new ThreadPoolExecutor.DiscardOldestPolicy()
+            new ThreadPoolExecutor.CallerRunsPolicy()
     );
     //使用线程池执行任务
     public static void execute(Runnable runnable){
