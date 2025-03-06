1683002eaf5bf7061a98dbf8b71dd33e55b08f52
ppx <zhusiyuanhao@163.com>
2025-03-06 12:23:07 +0800

    99

--- voice/run.sh
+++ voice/run.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+#####
+#这里是配置信息
+ENV_NAME="cosyvoice"         # 将这里的your_env_name替换为你要激活的conda环境名称
+PYTHON_SCRIPT_PATH=${DIR}"/server.py"  # 替换为你的Python脚本路径
+port=8000 # 根据端口号去查询对应的PID
+LOG_NAME="voice.log"
+######
+
+
+
+
+
+
+pid=$(sudo netstat -nlp | grep ":$port" | awk '{print $7}' | cut -d'/' -f1)
+# 杀掉对应的进程如果PID存在
+if [ -n "$pid" ]; then
+    sudo kill -9 $pid
+    echo "killed port:$port====> pid:$pid"
+fi
+
 
 # 定义你的Conda基础路径和虚拟环境名称
 CONDA_BASE=$(conda info --base)  # 获取conda的基础路径
-ENV_NAME="cosyvoice"         # 将这里的your_env_name替换为你要激活的conda环境名称
-PYTHON_SCRIPT_PATH="server.py"  # 替换为你的Python脚本路径
 
+ENV_PATH=${CONDA_BASE}/envs/${ENV_NAME}/bin/python
 # 检查是否已安装conda
 if ! command -v conda &> /dev/null; then
     echo "Conda could not be found. Please install Anaconda or Miniconda."
     exit 1
 fi
 
-# 初始化Conda环境变量
-__conda_setup="$('$CONDA_BASE/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "$CONDA_BASE/etc/profile.d/conda.sh" ]; then
-        . "$CONDA_BASE/etc/profile.d/conda.sh"
-    else
-        export PATH="$CONDA_BASE/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
-# 激活指定的conda环境
-source activate $ENV_NAME
-
-# 检查环境是否激活成功
-if [ $? -ne 0 ]; then
-    echo "Failed to activate Conda environment '$ENV_NAME'."
-    exit 1
-fi
-
+echo "current conda env is "
+conda info --envs
 # 运行Python脚本
-nohup python $PYTHON_SCRIPT_PATH> voice.log 2>&1 &
-
+nohup $ENV_PATH $PYTHON_SCRIPT_PATH> $LOG_NAME 2>&1 &
+echo "$ENV_PATH  $PYTHON_SCRIPT_PATH is running"
 
 
 
