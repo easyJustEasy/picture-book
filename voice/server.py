471c3d6020e947469c6c4136a51a682719833de8
ppx <zhusiyuanhao@163.com>
2025-03-08 10:21:41 +0800

    99

--- voice/server.py
+++ voice/server.py
@@ -1,5 +1,7 @@
-from fastapi import FastAPI, Request, Form, HTTPException, BackgroundTasks
+from fastapi import FastAPI, Request, Form, HTTPException
 from fastapi.responses import StreamingResponse, JSONResponse
+from starlette.background import BackgroundTask
+
 import os
 import torch
 from cosyvoice.cli.cosyvoice import CosyVoice2
@@ -11,18 +13,28 @@ from modelscope import snapshot_download
 from pathlib import Path
 import uvicorn
 import sys
-# 下载预训练模型
-model_dir = snapshot_download('iic/CosyVoice2-0.5B')
+import random
 
+from cosyvoice.utils.common import set_all_random_seed
+# 强制清理显存
+torch.cuda.empty_cache()
+torch.cuda.reset_peak_memory_stats()
+# 下载预训练模型
+model_dir = snapshot_download('iic/CosyVoice2-0.5B',local_dir='pretrained_models/CosyVoice2-0.5B')
+# model_dir = snapshot_download('iic/CosyVoice-300M',local_dir='pretrained_models/CosyVoice-300M')
 # 初始化CosyVoice2模型
 current_working_directory = str(Path(__file__).resolve().parent)
+temp_path = f'{current_working_directory}/temp'
+if not os.path.exists(temp_path):
+    os.mkdir()
 # 设置环境变量
 sys.path.append(f'{current_working_directory}/third_party/Matcha-TTS')
 sys.path.append(f'{current_working_directory}/cosyvoice')
 
 # 初始化 CosyVoice2 模型
 cosyvoice = CosyVoice2(
-       model_dir      # 使用半精度浮点数以减少显存占用
+       model_dir,
+       fp16=True
 )
 
 
@@ -34,20 +46,32 @@ def generate_data(model_output):
 
 app = FastAPI()
 
+def readFile(path):
+    with open("test.txt", "r") as f:  # 打开文件
+        data = f.read()  # 读取文件
+        return data
+
 
 @app.post("/get_voice_remote")
 async def get_voice_remote(request: Request):
-
+    seed = random.randint(1, 100000000)
+    set_all_random_seed(seed)
     # 强制清理显存
     torch.cuda.empty_cache()
     torch.cuda.reset_peak_memory_stats()
     form = await request.form()
     tts_text = form.get("tts_text")
     audio = form.get("audio")
-    if audio == '':
+    speed=form.get("speed")
+    if audio is None:
         audio = 'longyue'
-    prompt_speech_16k_r = load_wav(f'{current_working_directory}/asset/{audio}.wav', 16000)
-    model_output = cosyvoice.inference_instruct2(tts_text, '请用中文普通话朗读下面的儿童故事...', prompt_speech_16k_r)
+    if speed is None:
+        speed = 1.0
+    print(f'audio is {audio},speed is {speed}')
+    prompt_speech_16k_r = load_wav(f'{current_working_directory}/asset/{audio}/prompt.wav', 16000)
+
+    emo = readFile(f'{current_working_directory}/asset/{audio}/prompt.txt')
+    model_output = cosyvoice.inference_zero_shot(tts_text, emo, prompt_speech_16k_r,speed)
     tts_audio = b''
     for i, j in enumerate(model_output):
         tau = (j['tts_speech'].numpy() * (2 ** 15)).astype(np.int16).tobytes()
@@ -55,7 +79,9 @@ async def get_voice_remote(request: Request):
     tts_speech = torch.from_numpy(np.array(np.frombuffer(tts_audio, dtype=np.int16))).unsqueeze(dim=0)
     path = os.path.join(f'{current_working_directory}/temp', f'instruct_{uuid.uuid1()}.wav')
     torchaudio.save(path, tts_speech, cosyvoice.sample_rate)
-
+    # 强制清理显存
+    torch.cuda.empty_cache()
+    torch.cuda.reset_peak_memory_stats()
     async def iterfile():
         with open(path, mode="rb") as file_like:
             while True:
@@ -65,7 +91,7 @@ async def get_voice_remote(request: Request):
                 yield chunk
 
     return StreamingResponse(iterfile(), media_type="application/octet-stream",
-                             background=BackgroundTasks(lambda: os.remove(path)
+                             background=BackgroundTask(lambda: os.remove(path)
                                                         ))
 
 if __name__ == "__main__":
