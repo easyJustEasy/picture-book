581899e42e974d5e7c9a25c97764f33e30c137a3
siyuan.zhu <zhusiyuanhao@163.com>
2025-04-07 21:52:01 +0800

    99

--- voice/server.py
+++ voice/server.py
@@ -49,7 +49,7 @@ def generate_data(model_output):
 app = FastAPI()
 
 def readFile(path):
-    with open(path, "r") as f:  # 打开文件
+    with open(path, "r", encoding='utf-8') as f:  # 打开文件
         data = f.read()  # 读取文件
         return data
 
--- voice/test.py
+++ voice/test.py
@@ -1,5 +1,11 @@
-import torch
-# 强制清理显存
-torch.cuda.empty_cache()
-torch.cuda.reset_peak_memory_stats()
-print(f"当前 GPU 内存占用: {torch.cuda.memory_allocated() / 1e9:.2f} GB")
+# import torch
+# # 强制清理显存
+# torch.cuda.empty_cache()
+# torch.cuda.reset_peak_memory_stats()
+# print(f"当前 GPU 内存占用: {torch.cuda.memory_allocated() / 1e9:.2f} GB")
+
+def readFile(path):
+    with open(path, "r", encoding='utf-8') as f:  # 打开文件
+        data = f.read()  # 读取文件
+        return data
+print(readFile('./asset/海绵宝宝/prompt.txt'))
